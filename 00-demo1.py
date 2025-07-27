89285bf6a7d86b21264531ae7b5a73e4a101f44e
zhaojie <zj12november@foxmail.com>
2025-07-27 18:45:46 +0800

    update TAVULY API KEY

--- 00-demo1.py
+++ 00-demo1.py
@@ -13,5 +13,5 @@ _set_if_undefined("TAVILY_API_KEY")
 
 
 os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
-os.environ["TAVILY_API_KEY"] = os.environ.get("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")
+os.environ["TAVILY_API_KEY"] = os.environ.get("TAVILY_API_KEY")
 
