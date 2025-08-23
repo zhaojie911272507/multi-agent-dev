eba73c3851e47646011fab22236a6ccf2b82c452
zhaojie <zj12november@foxmail.com>
2025-08-23 14:02:12 +0800

    add an  AgenticRAG example

---
+++ langgraph_src/examples/AgenticRAG/rewrite_question.py
@@ -0,0 +1,48 @@
+from langchain_core.messages import convert_to_messages
+from langgraph.graph import MessagesState
+
+from langgraph_src.examples.AgenticRAG.generate_query import response_model
+
+REWRITE_PROMPT = (
+    "Look at the input and try to reason about the underlying semantic intent / meaning.\n"
+    "Here is the initial question:"
+    "\n ------- \n"
+    "{question}"
+    "\n ------- \n"
+    "Formulate an improved question:"
+)
+
+
+def rewrite_question(state: MessagesState):
+    """Rewrite the original user question."""
+    messages = state["messages"]
+    question = messages[0].content
+    prompt = REWRITE_PROMPT.format(question=question)
+    response = response_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [{"role": "user", "content": response.content}]}
+
+input = {
+    "messages": convert_to_messages(
+        [
+            {
+                "role": "user",
+                "content": "What does Lilian Weng say about types of reward hacking?",
+            },
+            {
+                "role": "assistant",
+                "content": "",
+                "tool_calls": [
+                    {
+                        "id": "1",
+                        "name": "retrieve_blog_posts",
+                        "args": {"query": "types of reward hacking"},
+                    }
+                ],
+            },
+            {"role": "tool", "content": "meow", "tool_call_id": "1"},
+        ]
+    )
+}
+
+response = rewrite_question(input)
+print(response["messages"][-1]["content"])
